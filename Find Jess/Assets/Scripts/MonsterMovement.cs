dfd596d6e3ac2c78ea2276958e06ec6bd23d20d0
ada <s2101097@edu.bc.fi>
2023-11-09 17:26:28 +0200

    new mosnter patrol
    - uusi ja eri patrolling koodi jos on mm. patrolling, chasing, ja stop metodit
    - navmesh juttuja

--- Find Jess/Assets/Scripts/MonsterMovement.cs
+++ Find Jess/Assets/Scripts/MonsterMovement.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class MonsterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    private void Start()
     {
-        
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+        UpdateAnimationUpdate();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateAnimationUpdate()
     {
-        
+
+        //idle ei toimi, koska speed on aina 8 pelissä.
+
+        if (speed > 0f)
+        {
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+        }
     }
 }
