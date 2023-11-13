56b975261c9abf8a1231a64a148dd1a0f4ce1ac7
ada <s2101097@edu.bc.fi>
2023-11-13 12:40:44 +0200

    sfx säätöjä
    - sfx scripti
    - audio player
    - 2 mp3:ta joita aion muokkaa myöhemmi

---
+++ Find Jess/Assets/Scripts/JessSFX.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Plays a randomized audio clip from 3 clips every once in a while
+/// </summary>
+
+public class JessSFX : MonoBehaviour
+{
+    public List<AudioClip> audioClips;
+    public AudioClip currentClip;
+    public AudioSource source;
+    public float minWaitBetweenPlays = 5f;
+    public float maxWaitBetweenPlays = 10f;
+    public float waitTimeCountdown = -1f;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            if (waitTimeCountdown < 0f)
+            {
+                currentClip = audioClips[UnityEngine.Random.Range(0, audioClips.Count)];
+                source.clip = currentClip;
+                source.Play();
+                waitTimeCountdown = UnityEngine.Random.Range(minWaitBetweenPlays, maxWaitBetweenPlays);
+            }
+            else
+            {
+                waitTimeCountdown -= Time.deltaTime;
+            }
+        }
+    }
+}
