14818196af137784d53842231d64efbdd1078938
onnitorkkeli <s2101095@edu.bc.fi>
2023-12-11 16:45:50 +0200

    jotai
    aa

---
+++ Find Jess/Assets/Mini First Person Controller/Scripts/DialogStarter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+using UnityEditor;
+using Unity.VisualScripting;
+
+public class DialogStarter : MonoBehaviour
+{
+    [SerializeField] private NPCConversation myConversation;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(Input.GetKey(KeyCode.E))
+            {
+                ConversationManager.Instance.StartConversation(myConversation);
+            }
+        }
+    }
+}
